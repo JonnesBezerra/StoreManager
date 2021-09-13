cd3a5b8e9f830f2fb12aac7287a0e8bc13759eb2
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-09-13 18:58:27 -0300

    Feat: create index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const productRouter = require('./routes/productRouter');
+
+const PORT = 3000;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/products', productRouter);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Pai tá on na porta ${PORT}`));
